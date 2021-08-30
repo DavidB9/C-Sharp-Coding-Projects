1daea5158f3a0673b498f4de5189474dde7cdb2c
David Buckingham <davidbuckingham95@gmail.com>
2021-08-30 16:39:35 +0100

    Committing Coding Projects

---
+++ Income Comparison Assignment/Income Comparison Assignment/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Income_Comparison_Assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            int person1HourlyRate = 15;
+            int person2HourlyRate = 20;
+
+            int person1HoursPerWeek = 40;
+            int person2HoursPerWeek = 40;
+
+            //calculate the salary for the year
+            int person1Salary = (person1HourlyRate * person1HoursPerWeek) * 52;
+            int person2Salary = (person2HourlyRate * person2HoursPerWeek) * 52;
+
+
+            Console.WriteLine("Anonymous Income Comparison Program");
+
+            //Person 1
+            Console.WriteLine("Person 1:");
+            Console.WriteLine("Hourly Rate?");
+            Console.WriteLine(person1HourlyRate);
+            Console.WriteLine("Hours worked per week?");
+            Console.WriteLine(person1HoursPerWeek);
+
+
+            //Person 2
+            Console.WriteLine("Person 2:");
+            Console.WriteLine("Hourly Rate?");
+            Console.WriteLine(person2HourlyRate);
+            Console.WriteLine("Hours worked per week?");
+            Console.WriteLine(person2HoursPerWeek);
+
+            //Annual Salary of Person 1
+            Console.WriteLine("Annual salary of Person 1:");
+            Console.WriteLine(person1Salary);
+
+            //Annual Salary of Person 2
+            Console.WriteLine("Annual salary of Person 2:");
+            Console.WriteLine(person2Salary);
+
+            //Comparision of annual salary
+            Console.WriteLine("Person 1 makes more money than Person 2");
+            bool moreMoney = person1Salary > person2Salary;
+            Console.WriteLine(moreMoney);
+            Console.Read();
+        }
+    }
+}
---
+++ PriceQuoteApp/PriceQuoteApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace PriceQuoteApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Package Express.Please follow the instructions below");
+
+            Console.WriteLine("Please enter the package weight:");
+            double weight = Convert.ToDouble(Console.ReadLine());
+
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day");
+            }
+            else
+            {
+                Console.WriteLine("Please enter the package width");
+                double width = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Please enter the package height");
+                double height = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Please enter the package length");
+                double length = Convert.ToDouble(Console.ReadLine());
+
+                double combine = width + height + length;
+
+                if(combine > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express");
+                }
+                else
+                {
+                    double quote = ((width * height * length) * weight) / 100 ;
+                    Console.WriteLine("Your estimated total for shipping this package is: £" + quote);
+                    Console.WriteLine("Thank you");   
+                    Console.Read();
+                }
+
+                
+            }
+        }
+    }
+}
